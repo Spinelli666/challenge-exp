70cd77b7bdf7db8fac758eb2a4d839ea21800bd8
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-12-05 11:16:18 -0300

    (feat): Add inicial do front

---
+++ exp-frontend/exp-crud/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import Home from './components/Home'
+import Experimento from './components/Experimento'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <Experimento />
+    </>
+  )
+}
+
+export default App
---
+++ exp-frontend/exp-crud/src/components/Experimento.jsx
@@ -0,0 +1,28 @@
+const Experimento = () => {
+    return (
+        <div>
+            <h2>Página para Cadastro de Experimento</h2>
+            <form>
+                <div>
+                    <label>Nome do Experimento: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Descrição: </label>
+                    <textarea placeholder="Descreva sobre o seu experimento."></textarea>
+                </div>
+                <div>
+                    <label>Data de Criação:  </label>
+                    <input type="date" />
+                </div>
+                <div>
+                    <label>Responsável: </label>
+                    <input type="text" />
+                </div>
+                <button type="submit">Criar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Experimento
