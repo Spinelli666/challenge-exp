22163926917a724d07bfb4b3d77703a315b4de37
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-12-06 12:25:17 -0300

    (feat): Add do Read

--- exp-frontend/exp-crud/src/components/Experimento.jsx
+++ exp-frontend/exp-crud/src/components/Experimento.jsx
@@ -1,29 +1,77 @@
+import { useState } from "react";
+
 const Experimento = () => {
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [dataCriacao, setDataCriacao] = useState("");
+    const [responsavel, setResponsavel] = useState("");
+
+    const addExperimento = async (e) => {
+        e.preventDefault(); 
+
+        const expData = {
+            nome,
+            descricao,
+            data: dataCriacao,
+            responsavel,
+        };
+
+        try {
+            const response = await fetch('http://localhost:8000/api/experimentos/create/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(expData),
+            });
+
+            const data = await response.json();
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <div>
             <h2>Página para Cadastro de Experimento</h2>
-            <form>
+            <form onSubmit={addExperimento}>
                 <div>
                     <label>Nome do Experimento: </label>
-                    <input type="text"/>
+                    <input 
+                        type="text" 
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)} 
+                    />
                 </div>
                 <div>
                     <label>Descrição: </label>
-                    <textarea placeholder="Descreva sobre o seu experimento."></textarea>
+                    <textarea 
+                        placeholder="Descreva sobre o seu experimento." 
+                        value={descricao}
+                        onChange={(e) => setDescricao(e.target.value)}
+                    ></textarea>
                 </div>
                 <div>
-                    <label>Data de Criação:  </label>
-                    <input type="date" />
+                    <label>Data de Criação: </label>
+                    <input 
+                        type="date"
+                        value={dataCriacao}
+                        onChange={(e) => setDataCriacao(e.target.value)} 
+                    />
                 </div>
                 <div>
                     <label>Responsável: </label>
-                    <input type="text" />
+                    <input 
+                        type="text"
+                        value={responsavel}
+                        onChange={(e) => setResponsavel(e.target.value)} 
+                    />
                 </div>
                 <button type="submit">Criar</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Experimento
+export default Experimento;
--- exp-frontend/exp-crud/src/components/Home.jsx
+++ exp-frontend/exp-crud/src/components/Home.jsx
@@ -1,36 +1,51 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+
+import "../styles/Home.css";
 
 const Home = () => {
-
-    const [experimento, setExperimento] = useState([])
+    const [experimento, setExperimento] = useState([]);
 
     useEffect(() => {
-        fetchExperimento()
-    }, [])
+        fetchExperimento();
+    }, []);
 
     const fetchExperimento = async () => {
         try {
             const response = await fetch('http://localhost:8000/api/experimentos/')
-            const data = await response.json()
-            setExperimento(data)
+            const data = await response.json();
+            setExperimento(data);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-    }
+    };
 
     return (
         <div>
             <h1>Home</h1>
-            {experimento.map((experimento) => (
-                <div key={experimento.experimento_id}>
-                    <p>Nome: {experimento.nome}</p>
-                    <p>Descrição: {experimento.descricao}</p>
-                    <p>Data: {experimento.data}</p>
-                    <p>Responsável: {experimento.responsavel}</p>
-                </div>
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Criado em</th>
+                        <th>Responsável</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {experimento.map((experimento) => (
+                        <tr>
+                            <td>{experimento.experimento_id}</td>
+                            <td>{experimento.nome}</td>
+                            <td>{experimento.descricao}</td>
+                            <td>{experimento.data}</td>
+                            <td>{experimento.responsavel}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
