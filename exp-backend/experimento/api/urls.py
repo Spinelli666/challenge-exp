2f8908b4a9b9aee24abb48a4a588f4ec8f94b6cd
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-12-10 11:59:57 -0300

    (feat): Add do PUT e DELETE

--- exp-backend/experimento/api/urls.py
+++ exp-backend/experimento/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_experimentos, create_experimentos
+from .views import get_experimentos, create_experimentos, experimentos_detail
 
 urlpatterns = [
     path('experimentos/', get_experimentos, name='get_experimentos'),
     path('experimentos/create/', create_experimentos, name='create_experimentos'),
+    path('experimentos/<int:pk>/', experimentos_detail, name='experimentos_detail'),
 ]
