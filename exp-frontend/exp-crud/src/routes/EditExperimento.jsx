016dc8ba81b727c9dd733eafe26995180fe3ab8c
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-12-11 16:00:47 -0300

    (feat): Add do ícone de lixo e css

--- exp-frontend/exp-crud/src/routes/EditExperimento.jsx
+++ exp-frontend/exp-crud/src/routes/EditExperimento.jsx
@@ -1,31 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const EditExperimento = () => {
-  return (
-    <div>
-      <h2>Página para Edição de Experimento</h2>
-      <button>Cancelar</button>
-      <form>
+    const { id } = useParams();
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [dataCriacao, setDataCriacao] = useState("");
+    const [responsavel, setResponsavel] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const navegar = useNavigate();
+
+    useEffect(() => {
+        fetchExperimento();
+    }, []);
+
+    const fetchExperimento = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/experimentos/${id}/`);
+            const data = await response.json();
+            setNome(data.nome);
+            setDescricao(data.descricao);
+            setDataCriacao(data.data);
+            setResponsavel(data.responsavel);
+            setIsLoading(false);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const updateExperimento = async (e) => {
+        e.preventDefault();
+
+        const expData = {
+            nome,
+            descricao,
+            data: dataCriacao,
+            responsavel,
+        };
+
+        try {
+            const response = await fetch(`http://localhost:8000/api/experimentos/${id}/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(expData),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            navegar('/');
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleCancel = () => {
+        navegar('/');
+    };
+
+    if (isLoading) {
+        return <div>Carregando...</div>;
+    }
+
+    return (
+        <div>
+            <h2>Página para Edição de Experimento</h2>
+            <button onClick={handleCancel}>Cancelar</button>
+            <form onSubmit={updateExperimento}>
                 <div>
                     <label>Nome do Experimento: </label>
-                    <input type="text"/>
+                    <input 
+                        type="text" 
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)} 
+                    />
                 </div>
                 <div>
                     <label>Descrição: </label>
-                    <textarea placeholder="Descreva sobre o seu experimento."></textarea>
+                    <textarea 
+                        placeholder="Descreva sobre o seu experimento." 
+                        value={descricao}
+                        onChange={(e) => setDescricao(e.target.value)}
+                    ></textarea>
                 </div>
                 <div>
                     <label>Data de Criação: </label>
-                    <input type="date"/>
+                    <input 
+                        type="date"
+                        value={dataCriacao}
+                        onChange={(e) => setDataCriacao(e.target.value)} 
+                    />
                 </div>
                 <div>
                     <label>Responsável: </label>
-                    <input type="text"/>
+                    <input 
+                        type="text"
+                        value={responsavel}
+                        onChange={(e) => setResponsavel(e.target.value)} 
+                    />
                 </div>
                 <button type="submit">Atualizar</button>
             </form>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default EditExperimento
+export default EditExperimento;
--- exp-frontend/exp-crud/src/routes/Home.jsx
+++ exp-frontend/exp-crud/src/routes/Home.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaEdit } from 'react-icons/fa';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 import "../styles/Home.css";
 
 const Home = () => {
     const [experimento, setExperimento] = useState([]);
-    const navegar = useNavigate()
+    const [searchTerm, setSearchTerm] = useState("");
+    const navegar = useNavigate();
 
     useEffect(() => {
         fetchExperimento();
@@ -14,26 +15,53 @@ const Home = () => {
 
     const fetchExperimento = async () => {
         try {
-            const response = await fetch('http://localhost:8000/api/experimentos/')
+            const response = await fetch('http://localhost:8000/api/experimentos/');
             const data = await response.json();
             setExperimento(data);
         } catch (err) {
             console.log(err);
         }
-    };
+    }
 
     const handleCriarExperimento = () => {
-        navegar('/experimento')
+        navegar('/experimento');
     }
 
     const handleEditExperimento = (id) => {
-        navegar(`/experimento/${id}`)
+        navegar(`/experimento/${id}`);
     }
 
+    const deleteExperimento = async (id) => {
+        try {
+            await fetch(`http://localhost:8000/api/experimentos/${id}/`, {
+                method: 'DELETE',
+            });
+
+            setExperimento((prev) => prev.filter((experimento) => experimento.experimento_id !== id));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const filteredExperimentos = experimento.filter((exp) =>
+        exp.nome.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        exp.data.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        exp.responsavel.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div>
-            <h1>Home</h1>
-            <button onClick={handleCriarExperimento}>Criar Experimento</button>
+        <div className="container">
+            <header>
+                <h1>Gerenciamento de Experimentos</h1>
+                <button className="create-button" onClick={handleCriarExperimento}>Criar Experimento</button>
+            </header>
+            <input
+                type="text"
+                className="search-bar"
+                placeholder="Pesquisar experimentos..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <table>
                 <thead>
                     <tr>
@@ -45,16 +73,19 @@ const Home = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {experimento.map((experimento) => (
-                        <tr>
+                    {filteredExperimentos.map((experimento) => (
+                        <tr key={experimento.experimento_id}>
                             <td>{experimento.experimento_id}</td>
                             <td>{experimento.nome}</td>
                             <td>{experimento.descricao}</td>
                             <td>{experimento.data}</td>
                             <td>{experimento.responsavel}</td>
                             <td>
-                                <button onClick={() => handleEditExperimento(experimento.experimento_id)}>
+                                <button className="action-button" onClick={() => handleEditExperimento(experimento.experimento_id)}>
                                     <FaEdit />
+                                </button>
+                                <button className="action-button" onClick={() => deleteExperimento(experimento.experimento_id)}>
+                                    <FaTrash />
                                 </button>
                             </td>
                         </tr>
