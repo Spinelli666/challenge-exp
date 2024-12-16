0456ff28c7eb246bea03140912ce75652cfc9e95
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-12-16 10:26:37 -0300

    (feat): Finalização

--- exp-frontend/exp-crud/src/routes/EditExperimento.jsx
+++ exp-frontend/exp-crud/src/routes/EditExperimento.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import ptBR from 'date-fns/locale/pt-BR';
+
+import "../styles/EditExperimento.css";
+
+// Registrar a localidade pt-BR
+registerLocale('pt-BR', ptBR);
+setDefaultLocale('pt-BR');
 
 const EditExperimento = () => {
     const { id } = useParams();
     const [nome, setNome] = useState("");
     const [descricao, setDescricao] = useState("");
-    const [dataCriacao, setDataCriacao] = useState("");
+    const [dataCriacao, setDataCriacao] = useState(null);
     const [responsavel, setResponsavel] = useState("");
+    const [trafego, setTrafego] = useState(100);
+    const [amostras, setAmostras] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const navegar = useNavigate();
 
@@ -20,8 +33,10 @@ const EditExperimento = () => {
             const data = await response.json();
             setNome(data.nome);
             setDescricao(data.descricao);
-            setDataCriacao(data.data);
+            setDataCriacao(new Date(data.data));
             setResponsavel(data.responsavel);
+            setTrafego(data.trafego);
+            setAmostras(data.amostras);
             setIsLoading(false);
         } catch (err) {
             console.log(err);
@@ -34,8 +49,14 @@ const EditExperimento = () => {
         const expData = {
             nome,
             descricao,
-            data: dataCriacao,
+            data: dataCriacao ? dataCriacao.toISOString().split('T')[0] : "",
             responsavel,
+            trafego,
+            amostras: amostras.map(amostra => ({
+                nome: amostra.nome,
+                descricao: amostra.descricao,
+                valor: parseFloat(amostra.valor)
+            })),
         };
 
         try {
@@ -59,16 +80,44 @@ const EditExperimento = () => {
         navegar('/');
     };
 
+    const handleAddAmostra = () => {
+        const newAmostras = [...amostras, { tipo: "alternativa", nome: "", descricao: "", valor: 0 }];
+        const totalAlternativas = newAmostras.reduce((acc, amostra) => amostra.tipo === 'alternativa' ? acc + parseFloat(amostra.valor) : acc, 0);
+        newAmostras[0].valor = 100 - totalAlternativas;
+        setAmostras(newAmostras);
+    };
+
+    const handleAmostraChange = (index, field, value) => {
+        const newAmostras = [...amostras];
+        newAmostras[index][field] = value;
+
+        if (field === 'valor' && newAmostras[index].tipo === 'alternativa') {
+            const totalAlternativas = newAmostras.reduce((acc, amostra) => amostra.tipo === 'alternativa' ? acc + parseFloat(amostra.valor) : acc, 0);
+            newAmostras[0].valor = 100 - totalAlternativas;
+        }
+
+        setAmostras(newAmostras);
+    };
+
+    const handleRemoveAmostra = (index) => {
+        const newAmostras = amostras.filter((_, i) => i !== index);
+        const totalAlternativas = newAmostras.reduce((acc, amostra) => amostra.tipo === 'alternativa' ? acc + parseFloat(amostra.valor) : acc, 0);
+        newAmostras[0].valor = 100 - totalAlternativas;
+        setAmostras(newAmostras);
+    };
+
     if (isLoading) {
         return <div>Carregando...</div>;
     }
 
     return (
-        <div>
-            <h2>Página para Edição de Experimento</h2>
-            <button onClick={handleCancel}>Cancelar</button>
+        <div className="container-fluid">
+            <div className="header">
+                <h2>Editar Experimento</h2>
+                <button className="back-button" onClick={handleCancel}>Cancelar</button>
+            </div>
             <form onSubmit={updateExperimento}>
-                <div>
+                <div className="form-group left-text">
                     <label>Nome do Experimento: </label>
                     <input 
                         type="text" 
@@ -76,7 +125,7 @@ const EditExperimento = () => {
                         onChange={(e) => setNome(e.target.value)} 
                     />
                 </div>
-                <div>
+                <div className="form-group left-text">
                     <label>Descrição: </label>
                     <textarea 
                         placeholder="Descreva sobre o seu experimento." 
@@ -84,23 +133,69 @@ const EditExperimento = () => {
                         onChange={(e) => setDescricao(e.target.value)}
                     ></textarea>
                 </div>
-                <div>
+                <div className="form-group form-group-inline">
                     <label>Data de Criação: </label>
-                    <input 
-                        type="date"
-                        value={dataCriacao}
-                        onChange={(e) => setDataCriacao(e.target.value)} 
+                    <DatePicker
+                        selected={dataCriacao}
+                        onChange={(date) => setDataCriacao(date)}
+                        dateFormat="dd/MM/yyyy"
+                        locale="pt-BR"
+                        placeholderText="Dia/Mês/Ano"
                     />
                 </div>
-                <div>
+                <div className="form-group form-group-inline">
                     <label>Responsável: </label>
                     <input 
                         type="text"
+                        placeholder="Nome Completo"
                         value={responsavel}
                         onChange={(e) => setResponsavel(e.target.value)} 
                     />
                 </div>
-                <button type="submit">Atualizar</button>
+                <div className="form-group form-group-inline">
+                    <label>Tráfego: </label>
+                    <input 
+                        type="number"
+                        value={trafego}
+                        onChange={(e) => setTrafego(parseInt(e.target.value))} 
+                    />
+                    <p>%</p>
+                </div>
+                <div className="form-group">
+                    <label>Amostras </label>
+                    <div className="amostras-container">
+                        {amostras.map((amostra, index) => (
+                        <div key={index} className="amostra-group">
+                            <label>{index === 0 ? "Controle" : `Alternativa ${index}`}: </label>
+                            <input 
+                                type="text"
+                                placeholder="Nome da Amostra"
+                                value={amostra.nome}
+                                onChange={(e) => handleAmostraChange(index, 'nome', e.target.value)} 
+                            />
+                            <input 
+                                type="text"
+                                placeholder="Descrição"
+                                value={amostra.descricao}
+                                onChange={(e) => handleAmostraChange(index, 'descricao', e.target.value)} 
+                            />
+                            <input 
+                                type="number"
+                                step="0.01"
+                                value={amostra.valor}
+                                onChange={(e) => handleAmostraChange(index, 'valor', parseFloat(e.target.value))} 
+                            />
+                            {index !== 0 && (
+                                <button type="button" className="remove-button" onClick={() => handleRemoveAmostra(index)}>
+                                    <FaTimes />
+                                </button>
+                            )}
+                        </div>
+                        ))}
+                        <button type="button" className="add-button" onClick={handleAddAmostra}>Adicionar Amostra</button>
+                    </div>
+                </div>
+                <button type="submit" className="submit-button">Atualizar</button>
             </form>
         </div>
     );
